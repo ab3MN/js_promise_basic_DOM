91798f7078629395bc22285f6c5444f27d570c7f
ab3MN <ab3mn3@gmail.com>
2024-10-11 08:19:12 +0300

    refactor:change createTextNode to createNotification,.add catch to promis1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,8 @@
 const logo = document.querySelector('.logo');
 const body = document.querySelector('body');
 
-const createTextNode = (type, parentNode, classes, text) => {
-  const node = document.createElement(type);
-
-  node.classList.add(...classes);
-  node.textContent = text;
-  parentNode.append(node);
-
-  return node;
-};
+const createNotification = (text, cssClass = '') =>
+  `<div class="message ${cssClass}">${text}</div>`;
 
 const promise1 = new Promise((resolve) => resolve('Promise was resolved!'));
 
@@ -20,11 +13,15 @@ const promise2 = new Promise((resolve, reject) => {
 });
 
 logo.addEventListener('click', () => {
-  promise1.then((res) => {
-    createTextNode('div', body, ['message'], res);
-  });
+  promise1
+    .then((res) => {
+      body.innerHTML += createNotification(res);
+    })
+    .catch((_error) => {
+      body.innerHTML += createNotification(_error.message, 'error-message');
+    });
 });
 
 promise2.catch((_error) => {
-  createTextNode('div', body, ['message', 'error-message'], _error.message);
+  body.innerHTML += createNotification(_error.message, 'error-message');
 });
